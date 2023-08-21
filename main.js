1c5816af743a3540d33a66fbece6a8cf5df13477
Nyaundi <jamesmabuka260@gmail.com>
2023-08-21 09:55:40 +0300

    This was fun

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke")
 
 const apiKey = "JOuDcgTg6a38ViSO2A6tYg==nUXIbkoW8FbMfgMG";
 const options = {
@@ -10,10 +11,17 @@ const options = {
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke(){
+
+    jokeEl.innerText = "Updating ..."
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading ..."
     const response = await fetch(apiURL, options);
     const data = await response.json();
 
-    console.log(data);
+    btnEl.disabled = false;
+    btnEl.innerText = "You Up for another Joke ..."
+
+    jokeEl.innerText = data[0].joke;
 }
 
 
